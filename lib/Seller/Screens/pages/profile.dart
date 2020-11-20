891436385295047d9d5ea46c05fc9fb0ac63185c
Astha Nayak <58568514+nayakastha@users.noreply.github.com>
2020-11-20 19:19:03 +0530

    profile

--- lib/Seller/Screens/pages/profile.dart
+++ lib/Seller/Screens/pages/profile.dart
@@ -6,6 +6,7 @@ import 'package:khojbuy/Seller/Services/navigator_bloc.dart';
 
 final CollectionReference users =
     FirebaseFirestore.instance.collection('SellerData');
+final snackbar = SnackBar(content: Text("Profile Updated !"));
 
 class ProfilePage extends StatefulWidget with NavigationStates {
   @override
@@ -29,6 +30,10 @@ class _ProfilePageState extends State<ProfilePage> {
         }
         if (snapshot.connectionState == ConnectionState.done) {
           Map<String, dynamic> data = snapshot.data.data();
+          String addLoc = data["AddressLocation"];
+          String addCity = data["AddressCity"];
+          bool delivery = data["Delivery"];
+          String deliveryAmt = data["MinAmt"];
           return SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -42,19 +47,36 @@ class _ProfilePageState extends State<ProfilePage> {
                         radius: 80,
                         child: Image.file(data["PhotoURL"]),
                       ),
-                Text(
-                  data["ShopName"],
-                  style: TextStyle(
-                    color: Color.fromRGBO(41, 74, 171, 1),
-                    fontSize: 25,
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    data["ShopName"],
+                    style: TextStyle(
+                      color: Color.fromRGBO(41, 74, 171, 1),
+                      fontSize: 25,
+                    ),
                   ),
                 ),
-                Text(
-                  data["Name"] + "   " + data["PhoneNo"],
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Color.fromRGBO(41, 74, 171, 0.6),
-                    letterSpacing: 2.5,
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    data["Name"] + "   " + data["PhoneNo"],
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color.fromRGBO(41, 74, 171, 0.6),
+                      letterSpacing: 2.5,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    data["Category"],
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color.fromRGBO(41, 74, 171, 0.6),
+                      letterSpacing: 2.5,
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -64,26 +86,125 @@ class _ProfilePageState extends State<ProfilePage> {
                     color: Colors.teal[100],
                   ),
                 ),
-                TextFormField(
-                  keyboardType: TextInputType.streetAddress,
-                  initialValue: data["AddressLocation"].toString(),
-                  decoration: new InputDecoration(
-                      contentPadding:
-                          EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32.0)),
-                      fillColor: Colors.white),
-                  validator: (val) {
-                    if (val.length == 0) {
-                      return "Address cannot be empty";
-                    } else {
-                      return null;
-                    }
-                  },
-                  onChanged: (val) {
-                    setState(() {});
-                  },
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.streetAddress,
+                    initialValue: addLoc,
+                    decoration: new InputDecoration(
+                        labelText: "Shop Location ",
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                        fillColor: Colors.white),
+                    validator: (val) {
+                      if (val.length == 0) {
+                        return "Address cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (val) {
+                      setState(() {
+                        addLoc = val;
+                      });
+                    },
+                  ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.streetAddress,
+                    initialValue: addCity,
+                    decoration: new InputDecoration(
+                        labelText: "Shop City",
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                        fillColor: Colors.white),
+                    validator: (val) {
+                      if (val.length == 0) {
+                        return "Address cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (val) {
+                      setState(() {
+                        addCity = val;
+                      });
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: SwitchListTile(
+                      title: Text(
+                        "Home Delivery Facility",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      value: delivery,
+                      onChanged: (val) {
+                        setState(() {
+                          delivery = val;
+                        });
+                      }),
+                ),
+                delivery
+                    ? Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextFormField(
+                          keyboardType: TextInputType.streetAddress,
+                          initialValue: deliveryAmt,
+                          decoration: new InputDecoration(
+                              labelText: "Minimum Amount for home delivery ",
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(32.0)),
+                              fillColor: Colors.white),
+                          validator: (val) {
+                            if (val.length == 0) {
+                              return "Amount cannot be empty";
+                            } else {
+                              return null;
+                            }
+                          },
+                          onChanged: (val) {
+                            setState(() {
+                              addCity = val;
+                            });
+                          },
+                        ),
+                      )
+                    : Container(),
+                Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 25),
+                    child: InkWell(
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          updateUserData(
+                              users, addLoc, addCity, delivery, deliveryAmt);
+                          Scaffold.of(context).showSnackBar(snackbar);
+                        },
+                        elevation: 10,
+                        backgroundColor: Color.fromRGBO(41, 74, 171, 0.6),
+                        label: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                          child: Text(
+                            "Save Data",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w900,
+                              fontFamily: 'Nunito',
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
               ],
             ),
           );
@@ -94,4 +215,18 @@ class _ProfilePageState extends State<ProfilePage> {
       },
     ));
   }
+}
+
+Future updateUserData(CollectionReference collectionReference, String addLoc,
+    String addCity, bool del, String minAmt) async {
+  return await collectionReference
+      .doc(FirebaseAuth.instance.currentUser.uid)
+      .set({
+    "AddressLocation": addLoc,
+    "AddressCity": addCity,
+    "Delivery": del,
+    "MinAmt": minAmt,
+  }).then((value) {
+    print("User Added");
+  }).catchError((error) => print(error));
 }
